1e3b8bf06f53aee62e6d8abe5486b399c19c3c3d
Patryk Marcisz <patryk.marcisz@gmail.com>
2019-07-08 21:06:33 +0200

    remove unused annotation

--- src/main/java/pl/sda/academy/javalon1/nbp/dto/currencies/CurrencyExchange.java
+++ src/main/java/pl/sda/academy/javalon1/nbp/dto/currencies/CurrencyExchange.java
@@ -7,7 +7,6 @@ import lombok.Data;
 import java.util.List;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CurrencyExchange {
     @JsonProperty("table")
     private String table;
